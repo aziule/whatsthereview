be3bb1fe6d7b9e57d73a2fe2d83a06f93f3a9d8f
William Claude <willclaude43@gmail.com>
2017-05-26 19:53:02 +0100

    Add sass

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,11 @@
 var path = require('path')
+var ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 module.exports = {
-    entry: './src/js/app.js',
+    entry: [
+        './src/js/app.js',
+        './src/sass/base.scss',
+    ],
     output: {
         path: path.resolve(__dirname, 'public/dist'),
         publicPath: 'dist/',
@@ -11,13 +15,20 @@ module.exports = {
         rules: [
             {
                 test: /\.vue$/,
-                loader: 'vue-loader',
+                use: 'vue-loader',
             },
             {
                 test: /\.js$/,
-                loader: 'babel-loader',
+                use: 'babel-loader',
                 exclude: /node_modules/
             },
+            {
+                test: /\.scss$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: "style-loader",
+                    use: "css-loader!sass-loader",
+                }),
+            }
         ]
     },
     resolve: {
@@ -28,4 +39,10 @@ module.exports = {
     devServer: {
         contentBase: './public'
     },
+    plugins: [
+        new ExtractTextPlugin({
+            filename: 'app.min.css',
+            allChunks: true,
+        }),
+    ],
 }
