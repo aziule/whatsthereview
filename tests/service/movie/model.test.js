30880c6ca44ddd2499fa4d03bd31952ef8423bfc
William Claude <william.claude@avantiplc.com>
2017-05-30 17:10:47 +0100

    Test the movie model

---
+++ tests/service/movie/model.test.js
@@ -0,0 +1,18 @@
+import Movie from '../../../src/js/service/movie/model'
+
+test('model creation', () => {
+    const type = Movie.TYPE_MOVIE;
+    const name = 'Avatar';
+    const pictureUrl = 'http://some-image-repo.com/image.png';
+    const score = 50;
+    const matchingScore = 100;
+
+    const movie = new Movie(type, name, pictureUrl, score, matchingScore);
+
+    expect(movie.type).toBe(type);
+    expect(movie.name).toBe(name);
+    expect(movie.pictureUrl).toBe(pictureUrl);
+    expect(movie.score).toBe(score);
+    expect(movie.matchingScore).toBe(matchingScore);
+});
+
