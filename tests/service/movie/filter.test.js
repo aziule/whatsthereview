6025a192a1ac6dd402340becc8813622843485f9
William Claude <william.claude@avantiplc.com>
2017-05-30 17:27:30 +0100

    Test the filter

---
+++ tests/service/movie/filter.test.js
@@ -0,0 +1,18 @@
+import Movie from '../../../src/js/service/movie/model'
+import Filter from '../../../src/js/service/movie/filter'
+
+test('sorts movies', () => {
+    const m1 = new Movie(Movie.TYPE_MOVIE, 'Silent Hill', 'pic', 50, 100);
+    const m2 = new Movie(Movie.TYPE_MOVIE, 'Hitch', 'pic', undefined, 100);
+    const m3 = new Movie(Movie.TYPE_TV_SHOW, 'World War Z', 'pic', undefined, 90);
+    const m4 = new Movie(Movie.TYPE_TV_SHOW, 'World War Z', 'pic', undefined, 0);
+
+    const movies = [m1, m2, m3, m4];
+
+    expect(
+        Filter.removeIrrelevantMovies(movies)
+    ).toEqual([
+        m1, m2
+    ]);
+});
+
