a2e52148ad8233ab53fc11cc667f6f070d0f0185
William Claude <willclaude43@gmail.com>
2017-05-30 19:49:48 +0100

    Test the evaluator

--- tests/service/movie/filter.test.js
+++ tests/service/movie/filter.test.js
@@ -5,7 +5,7 @@ test('sorts movies', () => {
     const m1 = new Movie(Movie.TYPE_MOVIE, 'Silent Hill', 'pic', 50, 100);
     const m2 = new Movie(Movie.TYPE_MOVIE, 'Hitch', 'pic', undefined, 100);
     const m3 = new Movie(Movie.TYPE_TV_SHOW, 'World War Z', 'pic', undefined, 90);
-    const m4 = new Movie(Movie.TYPE_TV_SHOW, 'World War Z', 'pic', undefined, 0);
+    const m4 = new Movie(Movie.TYPE_TV_SHOW, 'Welcome to Zombie Land', 'pic', undefined, 0);
 
     const movies = [m1, m2, m3, m4];
 
