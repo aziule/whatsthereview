befafe04405da30ad593b8cde4798ecfae39b7f0
William Claude <willclaude43@gmail.com>
2017-06-03 10:10:16 +0100

    Remove the FR lang choice as we will not add translations yet

--- src/js/component/lang-selector.vue
+++ src/js/component/lang-selector.vue
@@ -1,7 +1,6 @@
 <template>
     <section class="lang-selector">
         <div class="lang-selector__langs-list">
-            <span class="lang-selector__lang-item">FR</span>
             <span class="lang-selector__lang-item lang-selector__lang-item--active">EN</span>
         </div>
     </section>
