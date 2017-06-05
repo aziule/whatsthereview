61a48b2b1af60c145d983c6e87e638da8d51fb98
William Claude <willclaude43@gmail.com>
2017-06-05 10:16:56 +0100

    Test the search module

---
+++ tests/store/module/search.test.js
@@ -0,0 +1,25 @@
+"use strict"
+
+import SearchModule from '../../../src/js/store/module/search'
+
+it('sets the search query', () => {
+    const { setSearchQuery } = SearchModule.mutations;
+    const state = {
+        query: null
+    };
+
+    setSearchQuery(state, 'Die Hard');
+    expect(state.query).toBe('Die Hard');
+
+    setSearchQuery(state, 'Die Hard 2');
+    expect(state.query).toBe('Die Hard 2');
+});
+
+it('gets the current query', () => {
+    const state = {
+        query: 'The Sixth Sense'
+    };
+
+    const result = SearchModule.getters.currentQuery(state);
+    expect(result).toBe('The Sixth Sense');
+});
