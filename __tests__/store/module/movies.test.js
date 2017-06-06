2312015c6cb341f5201d17cc4a5eae540ec3c4fe
William Claude <willclaude43@gmail.com>
2017-06-06 10:36:36 +0100

    Wrap movies module tests into a describe block

--- __tests__/store/module/movies.test.js
+++ __tests__/store/module/movies.test.js
@@ -2,45 +2,47 @@
 
 import MoviesModule from '../../../src/js/store/module/movies'
 
-it('updates the movies list', () => {
-    const { updateMoviesList } = MoviesModule.mutations;
+describe('Movies module mutations', () => {
+    it('updates the movies list', () => {
+        const { updateMoviesList } = MoviesModule.mutations;
 
-    const state = {
-        movies: [],
-        wasUpdated: false
-    };
+        const state = {
+            movies: [],
+            wasUpdated: false
+        };
 
-    const movies = [
-        'Street Fighter',
-        'Double Impact',
-    ];
+        const movies = [
+            'Street Fighter',
+            'Double Impact',
+        ];
 
-    updateMoviesList(state, movies);
-    expect(state.movies).toBe(movies);
-    expect(state.wasUpdated).toBe(true);
-});
+        updateMoviesList(state, movies);
+        expect(state.movies).toBe(movies);
+        expect(state.wasUpdated).toBe(true);
+    });
 
-it('sets the loading state', () => {
-    const { setIsFetchingMovies, setIsNotFetchingMovies } = MoviesModule.mutations;
+    it('sets the loading state', () => {
+        const { setIsFetchingMovies, setIsNotFetchingMovies } = MoviesModule.mutations;
 
-    const state = {
-        isLoading: false
-    };
+        const state = {
+            isLoading: false
+        };
 
-    setIsFetchingMovies(state);
-    expect(state.isLoading).toBe(true);
+        setIsFetchingMovies(state);
+        expect(state.isLoading).toBe(true);
 
-    setIsNotFetchingMovies(state);
-    expect(state.isLoading).toBe(false);
-});
+        setIsNotFetchingMovies(state);
+        expect(state.isLoading).toBe(false);
+    });
 
-it('sets the error when fetching movies', () => {
-    const { setMoviesListError } = MoviesModule.mutations;
+    it('sets the error when fetching movies', () => {
+        const { setMoviesListError } = MoviesModule.mutations;
 
-    const state = {
-        error: null
-    };
+        const state = {
+            error: null
+        };
 
-    setMoviesListError(state, 'Oh noes :(');
-    expect(state.error).toBe('Oh noes :(');
+        setMoviesListError(state, 'Oh noes :(');
+        expect(state.error).toBe('Oh noes :(');
+    });
 });
